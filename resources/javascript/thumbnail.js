f7d79129e8da7d8fffb5ee0a64c87bdc4c3ba810
jamesread <contact@jwread.com>
2017-11-12 01:29:51 +0000

    show alt text

--- resources/javascript/thumbnail.js
+++ resources/javascript/thumbnail.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
 	$('img.thumbnail').on('click', function() {
 		var src = $(this).attr('src');
-		var fullImg = '<img src = "' + src.replace("200", "741") + '" /><p>Click the image to close.</p>'
+		var alt = $(this).attr('alt');
+		var fullImg = '<img src = "' + src.replace("200", "741") + '" /><p>' + alt + '</p>'
 
 		$('div#pictureViewer').addClass("shown");
 		$('div#pictureViewer').html(fullImg);
