7f523daa95742fb77115b80628c665cf722ccdba
jamesread <contact@jwread.com>
2017-11-12 01:06:42 +0000

    .

--- resources/javascript/thumbnail.js
+++ resources/javascript/thumbnail.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	$('img.thumbnail').on('click', function() {
 		var src = $(this).attr('src');
-		var fullImg = '<img src = "' . src . '" />'
+		var fullImg = '<img src = "' + src + '" />'
 
 		$('div#pictureViewer').modal();
 		$('div#pictureViewer').html(fullImg);
