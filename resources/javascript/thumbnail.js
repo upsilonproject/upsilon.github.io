a066237320e0d186086100b7e840b1ab920b3252
jamesread <contact@jwread.com>
2017-11-12 01:21:03 +0000

    stuff

--- resources/javascript/thumbnail.js
+++ resources/javascript/thumbnail.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	$('img.thumbnail').on('click', function() {
 		var src = $(this).attr('src');
-		var fullImg = '<img src = "' + src + '" />'
+		var fullImg = '<img src = "' + src.replace("200", "741") + '" /><p>Click to close.</p>'
 
 		$('div#pictureViewer').addClass("shown");
 		$('div#pictureViewer').html(fullImg);
